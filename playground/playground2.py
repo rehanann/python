878b742293514cf4fd6804865a8b3f443b26ff50
rehanwar <rehanwar@cisco.com>
2018-11-14 22:03:15 +0000

    update #2

--- playground/playground2.py
+++ playground/playground2.py
@@ -4,16 +4,21 @@ blockchain = []
 
 
 def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
+
     '''add last blockchain value in the list with [-1]'''
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     '''
     1. transaction_amount = ask user to input the amount in the array.
     2. last_transaction = this value starts with default amount of [1], and in the user input command 
         it is always included for the reference. 
     '''
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 # ask user to add the transaction amount values.
@@ -36,8 +41,8 @@ def print_blockchain():
 
 # get the first blockchain amount is the value entered by user.
 
-tx_amount = get_transaction_value()
-add_value(tx_amount)
+# tx_amount = get_transaction_value()
+# add_value(tx_amount)
 
 # tx_amount = get_user_input()
 # add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
@@ -52,7 +57,7 @@ while True:
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, last_transaction=get_last_blockchain_value())
+        add_transaction(tx_amount, last_transaction=get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain()
     elif user_choice == 'q':
@@ -60,5 +65,5 @@ while True:
     else:
         print('This is invalid option please select one')
     print('Choice Registered')
-    
+
 print('Done')
